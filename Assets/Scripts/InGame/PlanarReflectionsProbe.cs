284a637453f40a4ae9ceedf35937af3df024de93
Hyeon7 <kerter48@naver.com>
2023-10-09 23:52:35 +0900

    1009:2352
    스크립트 정리

--- Assets/Scripts/InGame/PlanarReflectionsProbe.cs
+++ Assets/Scripts/InGame/PlanarReflectionsProbe.cs
@@ -15,8 +15,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteAlways, AddComponentMenu("Rendering/Planar Reflections Probe")]
-public class PlanarReflectionsProbe : MonoBehaviour {
-
+public class PlanarReflectionsProbe : MonoBehaviour 
+{
     [Range(1, 4)] public int targetTextureID = 1;
     [Space(10)]
     public bool useCustomNormal = false;
@@ -31,26 +31,28 @@ public class PlanarReflectionsProbe : MonoBehaviour {
     private GameObject _probeGO;
     private Camera _probe;
     private Skybox _probeSkybox;
-    private Dictionary<Camera, RenderTexture> _camTextureMap =
-        new Dictionary<Camera, RenderTexture>();
+    private Dictionary<Camera, RenderTexture> _camTextureMap = new Dictionary<Camera, RenderTexture>();
     private ArrayList _ignoredCameras = new ArrayList();
-
-
-    private void OnEnable () {
+    
+    private void OnEnable () 
+    {
         Camera.onPreRender += PreRenderRoutine;
     }
 
-    private void OnDisable () {
+    private void OnDisable () 
+    {
         FinalizeProbe();
         Camera.onPreRender -= PreRenderRoutine;
     }
 
-    private void OnDestroy () {
+    private void OnDestroy () 
+    {
         FinalizeProbe();
         Camera.onPreRender -= PreRenderRoutine;
     }
 
-    private void InitializeProbe () {
+    private void InitializeProbe () 
+    {
         _probeGO = new GameObject("", typeof(Camera), typeof(Skybox));
         _probeGO.name = "PRCamera" + _probeGO.GetInstanceID().ToString();
         _probeGO.hideFlags = HideFlags.HideAndDontSave;
@@ -60,44 +62,57 @@ public class PlanarReflectionsProbe : MonoBehaviour {
         _probeSkybox.material = null;
     }
 
-    private void FinalizeProbe () {
+    private void FinalizeProbe () 
+    {
         CleanupRenderTextures();
-        if (_probe == null) {
+        if (_probe == null) 
+        {
             return;
         }
-        if (Application.isEditor) {
+        if (Application.isEditor) 
+        {
             DestroyImmediate(_probeGO);
         }
-        else {
+        else 
+        {
             Destroy(_probeGO);
         }
     }
 
-    private void CleanupRenderTextures() {
-        foreach (RenderTexture texture in _camTextureMap.Values) {
+    private void CleanupRenderTextures() 
+    {
+        foreach (RenderTexture texture in _camTextureMap.Values) 
+        {
             texture.Release();
         }
         _camTextureMap.Clear();
     }
 
-    private bool CheckCamera (Camera cam) {
-        if (cam.cameraType == CameraType.Reflection) {
+    private bool CheckCamera (Camera cam) 
+    {
+        if (cam.cameraType == CameraType.Reflection) 
+        {
             return true;
         }
-        else if (!renderInEditor && cam.cameraType == CameraType.SceneView) {
+        else if (!renderInEditor && cam.cameraType == CameraType.SceneView) 
+        {
             return true;
         }
-        else if (_ignoredCameras.Contains(cam)) {
+        else if (_ignoredCameras.Contains(cam)) 
+        {
             return true;
         }
         return false;
     }
 
-    private void PreRenderRoutine (Camera cam) {
-        if (CheckCamera(cam)) {
+    private void PreRenderRoutine (Camera cam) 
+    {
+        if (CheckCamera(cam)) 
+        {
             return;
         }
-        else if (_probe == null) {
+        else if (_probe == null) 
+        {
             InitializeProbe();
         }
         Vector3 normal = GetNormal();
@@ -110,7 +125,8 @@ public class PlanarReflectionsProbe : MonoBehaviour {
         _probe.targetTexture.SetGlobalShaderProperty(texName);
     }
 
-    private void UpdateProbeSettings (Camera cam) {
+    private void UpdateProbeSettings (Camera cam) 
+    {
         _probe.CopyFrom(cam);
         _probe.enabled = false;
         _probe.cameraType = CameraType.Reflection;
@@ -118,25 +134,31 @@ public class PlanarReflectionsProbe : MonoBehaviour {
         _probe.farClipPlane = farClipPlane;
         _probeSkybox.material = null;
         _probeSkybox.enabled = false;
-        if (renderBackground) {
+        if (renderBackground) 
+        {
             _probe.clearFlags = cam.clearFlags;
-            if (cam.GetComponent<Skybox>()) {
+            if (cam.GetComponent<Skybox>()) 
+            {
                 Skybox camSkybox = cam.GetComponent<Skybox>();
                 _probeSkybox.material = camSkybox.material;
                 _probeSkybox.enabled = camSkybox.enabled;
             }
         }
-        else {
+        else 
+        {
             _probe.clearFlags = CameraClearFlags.Nothing;
         }
     }
 
-    private void CreateRenderTexture(Camera cam) {
+    private void CreateRenderTexture(Camera cam) 
+    {
         int width = (int)((float)cam.pixelWidth * reflectionsQuality);
         int height = (int)((float)cam.pixelHeight * reflectionsQuality);
         RenderTexture texture = _camTextureMap.GetValueOrDefault(cam, null);
-        if (!texture || texture.width != width || texture.height != height) {
-            if (texture) {
+        if (!texture || texture.width != width || texture.height != height) 
+        {
+            if (texture) 
+            {
                 _camTextureMap.Remove(cam);
                 texture.Release();
             }
@@ -144,84 +166,98 @@ public class PlanarReflectionsProbe : MonoBehaviour {
             _probe.targetTexture.Create();
             _camTextureMap.Add(cam, _probe.targetTexture);
         }
-        else {
+        else 
+        {
             _probe.targetTexture = texture;
         }
     }
 
-    private Vector3 GetNormal () {
-        if (!useCustomNormal) {
+    private Vector3 GetNormal () 
+    {
+        if (!useCustomNormal) 
+        {
             return transform.forward;
         }
-        else if (customNormal.Equals(Vector3.zero)) {
+        else if (customNormal.Equals(Vector3.zero)) 
+        {
             return Vector3.up;
         }
-        else {
+        else 
+        {
             return customNormal.normalized;
         }
     }
 
     // The probe's camera position should be the the current camera's position
     // mirrored by the reflecting plane. Its rotation mirrored too.
-    private void UpdateProbeTransform (Camera cam, Vector3 normal) {
-        Vector3 proj = normal * Vector3.Dot(
-            normal, cam.transform.position - transform.position);
+    private void UpdateProbeTransform (Camera cam, Vector3 normal) 
+    {
+        Vector3 proj = normal * Vector3.Dot(normal, cam.transform.position - transform.position);
         _probe.transform.position = cam.transform.position - 2 * proj;
         Vector3 probeForward = Vector3.Reflect(cam.transform.forward, normal);
         Vector3 probeUp = Vector3.Reflect(cam.transform.up, normal);
-        _probe.transform.LookAt(
-            _probe.transform.position + probeForward, probeUp);
+        _probe.transform.LookAt(_probe.transform.position + probeForward, probeUp);
     }
 
     // The clip plane should coincide with the plane with reflections.
-    private void CalculateObliqueProjection (Vector3 normal) {
+    private void CalculateObliqueProjection (Vector3 normal) 
+    {
         Matrix4x4 viewMatrix = _probe.worldToCameraMatrix;
         Vector3 viewPosition = viewMatrix.MultiplyPoint(transform.position);
         Vector3 viewNormal = viewMatrix.MultiplyVector(normal);
-        Vector4 plane = new Vector4(
-            viewNormal.x, viewNormal.y, viewNormal.z,
-            -Vector3.Dot(viewPosition, viewNormal));
+        Vector4 plane = new Vector4(viewNormal.x, viewNormal.y, viewNormal.z, -Vector3.Dot(viewPosition, viewNormal));
         _probe.projectionMatrix = _probe.CalculateObliqueMatrix(plane);
     }
 
 
-    public void IgnoreCamera (Camera cam) {
-        if (!_ignoredCameras.Contains(cam)) {
+    public void IgnoreCamera (Camera cam) 
+    {
+        if (!_ignoredCameras.Contains(cam)) 
+        {
             _ignoredCameras.Add(cam);
         }
     }
 
-    public void UnignoreCamera (Camera cam) {
-        if (_ignoredCameras.Contains(cam)) {
+    public void UnignoreCamera (Camera cam) 
+    {
+        if (_ignoredCameras.Contains(cam))
+        {
             _ignoredCameras.Remove(cam);
         }
     }
 
-    public void ClearIgnoredList () {
+    public void ClearIgnoredList () 
+    {
         _ignoredCameras.Clear();
     }
 
-    public bool IsIgnoring (Camera cam) {
+    public bool IsIgnoring (Camera cam) 
+    {
         return _ignoredCameras.Contains(cam);
     }
 
 
-    public static PlanarReflectionsProbe[] FindProbesRenderingTo (int id) {
+    public static PlanarReflectionsProbe[] FindProbesRenderingTo (int id) 
+    {
         var probes = FindObjectsOfType<PlanarReflectionsProbe>();
         ArrayList list = new ArrayList();
-        foreach (PlanarReflectionsProbe probe in probes) {
-            if (probe.targetTextureID == id) {
+        foreach (PlanarReflectionsProbe probe in probes)
+        {
+            if (probe.targetTextureID == id) 
+            {
                 list.Add(probe);
             }
         }
-        return (PlanarReflectionsProbe[])list.ToArray(
-            typeof(PlanarReflectionsProbe));
+        return (PlanarReflectionsProbe[])list.ToArray(typeof(PlanarReflectionsProbe));
     }
 
-    public static PlanarReflectionsProbe FindProbeRenderingTo (int id) {
+    public static PlanarReflectionsProbe FindProbeRenderingTo (int id) 
+    {
         var probes = FindObjectsOfType<PlanarReflectionsProbe>();
-        foreach (PlanarReflectionsProbe probe in probes) {
-            if (probe.targetTextureID == id) {
+        foreach (PlanarReflectionsProbe probe in probes) 
+        {
+            if (probe.targetTextureID == id) 
+            {
                 return probe;
             }
         }
