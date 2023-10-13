3bb62e36ddd03adedd81eba6a84c0e0251b2b66d
Hyeon7 <kerter48@naver.com>
2023-10-13 10:55:35 +0900

    1013:1055
    -오브젝트 펄스 추가
    -비트에 따른 펄스 관리 추가

---
+++ Assets/Scripts/InGame2/Audio Manager.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private float bpm;
+    
+    [SerializeField]
+    private AudioSource audio;
+    
+    [SerializeField]
+    private Intervals[] intervals;
+
+    private void Update()
+    {
+        foreach (Intervals interval in intervals)
+        {
+            float sampledTime = (audio.timeSamples / (audio.clip.frequency * interval.GetIntervalLength(bpm)));
+            interval.CheckForNewInterval(sampledTime);
+        }
+    }
+
+    void Start()
+    {
+        Invoke(nameof(PlayMusic), 1f);
+    }
+
+    void PlayMusic()
+    {
+        
+        audio.Play();
+    }
+}
+
+[Serializable]
+public class Intervals
+{
+    [SerializeField]
+    private float steps;
+    [SerializeField]
+    private UnityEvent trigger;
+    private int lastInterval;
+    
+    public float GetIntervalLength(float bpm)
+    {
+        return 60f / (bpm * steps);
+    }
+
+    public void CheckForNewInterval(float interval)
+    {
+        if (Mathf.FloorToInt(interval) != lastInterval)
+        {
+            lastInterval = Mathf.FloorToInt(interval);
+            trigger.Invoke();
+        }
+    }
+}
